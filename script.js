64996aeda9a8b0a58c774d0ec09fae223f7ba26b
Manjunath <66263712+Manjunath1299@users.noreply.github.com>
2023-12-25 18:52:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 const sampleObject = { red: "#FF0000", green: "#00FF00", white: "#FFFFFF" };
-
+let obj=Object.keys(sampleObject);
+// console.log(obj+" "+typeof(obj));
 function hasKey(key) {
   //   write your code here
+  for( let i=0;i<obj.length;i++){
+      if(obj[i]===key)
+       return true
+    //   console.log(obj[i]);
+  }
+	
+	return false;
 }
 
 // Do not change the code below
